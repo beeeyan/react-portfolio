671677b682e9feaa73f7a234cb35ed5aaba86706
sakasanokuma <moonsunphoenix@gmail.com>
2021-05-04 14:49:57 +0900

    add emptySpace idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {useInteractJS} from './hooks';
@@ -34,21 +35,174 @@ import {useInteractJS} from './hooks';
  * @return {React.FC}
  */
 const App: React.FC = () => {
-  const interact = useInteractJS();
+  const initPositionB = {
+    width: 100,
+    height: 100,
+    x: 100,
+    y: 0,
+  };
+  const initPositionC = {
+    width: 100,
+    height: 100,
+    x: 200,
+    y: 0,
+  };
+  const initPositionD = {
+    width: 100,
+    height: 100,
+    x: 0,
+    y: 100,
+  };
+  const initPositionE = {
+    width: 100,
+    height: 100,
+    x: 100,
+    y: 100,
+  };
+  const initPositionF = {
+    width: 100,
+    height: 100,
+    x: 200,
+    y: 100,
+  };
+  const initPositionG = {
+    width: 100,
+    height: 100,
+    x: 0,
+    y: 200,
+  };
+  const initPositionH = {
+    width: 100,
+    height: 100,
+    x: 100,
+    y: 200,
+  };
+  const initPositionJ = {
+    width: 100,
+    height: 100,
+    x: 300,
+    y: 200,
+  };
+  const interactA = useInteractJS();
+  const interactB = useInteractJS(initPositionB);
+  const interactC = useInteractJS(initPositionC);
+  const interactD = useInteractJS(initPositionD);
+  const interactE = useInteractJS(initPositionE);
+  const interactF = useInteractJS(initPositionF);
+  const interactG = useInteractJS(initPositionG);
+  const interactH = useInteractJS(initPositionH);
+  const interactJ = useInteractJS(initPositionJ);
+
+  useEffect(() => {
+    // interactG.disable();
+    // console.log(interactJ.positionName);
+    console.log('test');
+  }, [interactJ.position]);
+
 
   return (
     <div className='App'>
-      <button onClick={() => interact.enable()}>有効化</button>
-      <button onClick={() => interact.disable()}>無効化</button>
+      <button onClick={() => interactA.enable()}>有効化</button>
+      <button onClick={() => interactA.disable()}>無効化</button>
+      {/* A (0,0) */}
       <div
-        ref={interact.ref}
+        ref={interactA.ref}
         style={{
-          ...interact.style,
+          ...interactA.style,
           border: '2px solid #0489B1',
           backgroundColor: '#A9D0F5',
         }}
       >
-
+        A
+      </div>
+      {/* B (100,0) */}
+      <div
+        ref={interactB.ref}
+        style={{
+          ...interactB.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        B
+      </div>
+      {/* C (200,0) */}
+      <div
+        ref={interactC.ref}
+        style={{
+          ...interactC.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        C
+      </div>
+      {/* D (0,100) */}
+      <div
+        ref={interactD.ref}
+        style={{
+          ...interactD.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        D
+      </div>
+      {/* E (100,100) */}
+      <div
+        ref={interactE.ref}
+        style={{
+          ...interactE.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        E
+      </div>
+      {/* F (200,100) */}
+      <div
+        ref={interactF.ref}
+        style={{
+          ...interactF.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        F
+      </div>
+      {/* G (0,200) */}
+      <div
+        ref={interactG.ref}
+        style={{
+          ...interactG.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        G
+      </div>
+      {/* H (100,200) */}
+      <div
+        ref={interactH.ref}
+        style={{
+          ...interactH.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        H
+      </div>
+      {/* I (200,200) */}
+      <div
+        ref={interactJ.ref}
+        style={{
+          ...interactJ.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5',
+        }}
+      >
+        {interactJ.position.x}
+        I
       </div>
     </div>
   );
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,22 +1,91 @@
 import {CSSProperties, useEffect, useRef, useState} from 'react';
 import interact from 'interactjs';
 
-type Partial<T> = {
-    [P in keyof T]?: T[P];
+// type Partial<T> = {
+//     [P in keyof T]?: T[P];
+// }
+
+type Position = {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
 }
 
-const initPosition = {
+const initPosition : Position = {
   width: 100,
   height: 100,
   x: 0,
   y: 0,
 };
 
-const edgePosition = {
-  maxX: 200,
-  maxY: 200,
+const edge = {
   minX: 0,
   minY: 0,
+  maxX: 200,
+  maxY: 200,
+};
+
+const emptySpace = {
+  minX: 200,
+  minY: 200,
+  maxX: 300,
+  maxY: 300,
+};
+
+/**
+ * 現在のpositionを判断する関数
+ * @param {number} x
+ * @param {number} y
+ * @return {String}
+ */
+const judgePosition = (x: number, y: number): String => {
+  const puzzlePositon = {
+    a: {x: 0, y: 0},
+    b: {x: 100, y: 0},
+    c: {x: 200, y: 0},
+    d: {x: 0, y: 100},
+    e: {x: 100, y: 100},
+    f: {x: 200, y: 100},
+    g: {x: 0, y: 200},
+    h: {x: 100, y: 200},
+    i: {x: 200, y: 200},
+    j: {x: 300, y: 200},
+  };
+
+  if (puzzlePositon.a.x === x && puzzlePositon.a.y === y) {
+    return 'A';
+  } else if (puzzlePositon.b.x === x &&
+                puzzlePositon.b.y === y) {
+    return 'B';
+  } else if (puzzlePositon.c.x === x &&
+                puzzlePositon.c.y === y) {
+    return 'C';
+  } else if (puzzlePositon.d.x === x &&
+                puzzlePositon.d.y === y) {
+    return 'D';
+  } else if (puzzlePositon.e.x === x &&
+                puzzlePositon.e.y === y) {
+    return 'E';
+  } else if (puzzlePositon.f.x === x &&
+                puzzlePositon.f.y === y) {
+    return 'F';
+  } else if (puzzlePositon.g.x === x &&
+                puzzlePositon.g.y === y) {
+    return 'G';
+  } else if (puzzlePositon.h.x === x &&
+                puzzlePositon.h.y === y) {
+    return 'H';
+  } else if (puzzlePositon.i.x === x &&
+                puzzlePositon.i.y === y) {
+    return 'I';
+  } else if (puzzlePositon.j.x === x &&
+                puzzlePositon.j.y === y) {
+    return 'J';
+  } else {
+    // 途中
+    return 'midway';
+  }
 };
 
 /**
@@ -28,8 +97,7 @@ const edgePosition = {
  * @return {interactRef}
  */
 export function useInteractJS(
-    position: Partial<typeof initPosition> = initPosition,
-    edge: Required<typeof edgePosition> = edgePosition,
+    position: Position = initPosition,
 ) {
   const [_position, setPosition] = useState({
     ...initPosition,
@@ -53,20 +121,60 @@ export function useInteractJS(
           inertia: false,
         })
         .on('dragmove', (event) => {
-          x += event.dx;
-          y += event.dy;
-          // confirm max position
-          x = x >= edge.maxX? edge.maxX : x;
-          y = y >= edge.maxY? edge.maxY : y;
-          // confirm min position
-          x = x <= edge.minX? edge.minX : x;
-          y = y <= edge.minY? edge.minY : y;
-          setPosition({
-            width,
-            height,
-            x,
-            y,
-          });
+          if (emptySpace.minX === x) {
+            edge.minX = emptySpace.minX;
+            edge.maxX = emptySpace.minX;
+            if ((emptySpace.minY - 100) <= y &&
+                  y < emptySpace.minY) {
+              edge.minY = emptySpace.minY - 100;
+              edge.maxY = emptySpace.minY;
+            }
+            if (emptySpace.minY < y &&
+                y <= (emptySpace.minY + 100)) {
+              edge.minY = emptySpace.minY;
+              edge.maxY = emptySpace.minY + 100;
+            }
+          }
+          if (emptySpace.minY === y) {
+            edge.minY = emptySpace.minY;
+            edge.maxY = emptySpace.minY;
+            if ((emptySpace.minX - 100) <= x &&
+                  x < emptySpace.minX) {
+              edge.minX = emptySpace.minX - 100;
+              edge.maxX = emptySpace.minX;
+            }
+            if (emptySpace.minX < x &&
+                x <= (emptySpace.minX + 100)) {
+              edge.minX = emptySpace.minX;
+              edge.maxX = emptySpace.minX + 100;
+            }
+          }
+          if (edge.minX <= x && x <= edge.maxX &&
+                edge.minY <= y && y <= edge.maxY) {
+            x += event.dx;
+            y += event.dy;
+            // confirm max position
+            x = x >= edge.maxX? edge.maxX : x;
+            y = y >= edge.maxY? edge.maxY : y;
+            // confirm min position
+            x = x <= edge.minX? edge.minX : x;
+            y = y <= edge.minY? edge.minY : y;
+            setPosition({
+              width,
+              height,
+              x,
+              y,
+            });
+          }
+          // emptySpaceの更新
+          const tempPositionName = judgePosition(x, y);
+          console.log(tempPositionName);
+          if (emptySpace.minX === x && emptySpace.minY === y) {
+            emptySpace.minX = (emptySpace.minX === edge.minX)?
+                                 edge.maxX : edge.minX;
+            emptySpace.minY = (emptySpace.minY === edge.minY)?
+                                 edge.maxY : edge.minY;
+          }
         });
     // .on('resizemove', (event) =>{
     //   width = event.rect.width;
@@ -103,6 +211,7 @@ export function useInteractJS(
       height: _position.height + 'px',
       position: 'absolute' as CSSProperties['position'],
     },
+    positionName: judgePosition(_position.x, _position.y),
     position: _position,
     isEnabled,
     enable: () => setEnable(true),
