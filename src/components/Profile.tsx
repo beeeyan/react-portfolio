6abbe6640ee5026addebb99a3dbe495aa41d1dfc
sakasanokuma <moonsunphoenix@gmail.com>
2021-08-02 17:00:31 +0900

    add profile title

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -22,33 +22,36 @@ const options: Options = {
  */
 const Profile: React.FC<IProfileFields> = (props: IProfileFields) => {
   return (
-    <Card sx={{'width': '500px'}}>
-      <div sx={{textAlign: 'center'}}>
-        <h2>NAME : {props.name}</h2>
-        <Image
-          src={props.image.fields.file.url}
-          alt={props.image.fields.title}/>
-      </div>
-      <h4 sx={{mb: -2}}>自己紹介文</h4>
-      {documentToReactComponents(props.content, options)}
-      <h4 sx={{mb: 1}}>保有資格</h4>
-      {props.qualifications &&
-         props.qualifications.items.map((item: string, index: number) => (
-           <div key={`Qualification${index}`}>{item}</div>
-         ))
-      }
-      <h4 sx={{mb: 1}}>触れたことのある技術</h4>
-      {props.skills &&
-         props.skills.items.map((item: string, index: number) => (
-           <div key={`skill${index}`}>{item}</div>
-         ))
-      }
-      <h4 sx={{mb: 1}}>このサイトについて</h4>
-      React × TypeScriptで作成。
-      ポートフォリオの役割を担っており、あまり細かいことは書きませんでした。<br/>
-      より詳しい（雑多な）情報は以下にまとめています。<br/>
-      Scrapbox : <a href='https://scrapbox.io/beeeyan-trial-box/' target='_blank' rel='noreferrer'>beeeyan-trial-box</a>
-    </Card>
+    <div>
+      <h2>Profile</h2>
+      <Card sx={{'width': '500px'}}>
+        <div sx={{textAlign: 'center'}}>
+          <h2>NAME : {props.name}</h2>
+          <Image
+            src={props.image.fields.file.url}
+            alt={props.image.fields.title}/>
+        </div>
+        <h4 sx={{mb: -2}}>自己紹介文</h4>
+        {documentToReactComponents(props.content, options)}
+        <h4 sx={{mb: 1}}>保有資格</h4>
+        {props.qualifications &&
+           props.qualifications.items.map((item: string, index: number) => (
+             <div key={`Qualification${index}`}>{item}</div>
+           ))
+        }
+        <h4 sx={{mb: 1}}>触れたことのある技術</h4>
+        {props.skills &&
+           props.skills.items.map((item: string, index: number) => (
+             <div key={`skill${index}`}>{item}</div>
+           ))
+        }
+        <h4 sx={{mb: 1}}>このサイトについて</h4>
+        React × TypeScriptで作成。
+        ポートフォリオの役割を担っており、あまり細かいことは書きませんでした。<br/>
+        より詳しい（雑多な）情報は以下にまとめています。<br/>
+        Scrapbox : <a href='https://scrapbox.io/beeeyan-trial-box/' target='_blank' rel='noreferrer'>beeeyan-trial-box</a>
+      </Card>
+    </div>
   );
 };
 
